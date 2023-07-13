040cc52f2f4e4d42f2ba7b3e9c1ccf0c70c81695
VishwajitTD <vishwajitdongle@gmail.com>
2023-07-13 18:00:54 +0530

    1243142534162

---
+++ src/test/java/pgs/hms/test/SampleTest.java
@@ -0,0 +1,12 @@
+package pgs.hms.test;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest() {
+		System.out.println("Jenkind is not running");
+	}
+	
+}
